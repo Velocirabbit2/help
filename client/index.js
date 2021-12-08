02f276d61b4b42a196fb5fcefc900e578ca57609
KCrandall6 <kcrandall67@gmail.com>
2021-12-07 21:18:58 -0700

    webpack and boilerplate

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import styles from './client/assets/style.css';
+// import App from './App.js';
+
+ReactDOM.render(<h1>BRYAN!</h1>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './client/index.html'),
+    }),
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.(jsx?)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './client/index.html'),
+        }),
+    ],
+    devServer: {
+      compress: true,
+      port: 8080,
+      hot: true,
+      proxy: {
+        '/api': 'http://localhost:3000',
+        secure: true,
+      },
+      static: {
+        directory: path.join(__dirname, '/'),
+        publicPath: '/',
+      },
+    }
+};
